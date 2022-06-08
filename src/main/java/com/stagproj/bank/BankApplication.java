594cc7610f0443bc201ac269a92255024b263e12
João Tavares <joaostavares@outlook.com>
2022-06-08 13:23:42 -0300

    Algumas correções antes de inserir o controller funcionando

--- src/main/java/com/stagproj/bank/BankApplication.java
+++ src/main/java/com/stagproj/bank/BankApplication.java
@@ -13,21 +13,3 @@ public class BankApplication {
 	}
 
 }
-//		***************************
-//		APPLICATION FAILED TO START
-//		***************************
-//
-//		Description:
-//
-//		Field _contaRepository in com.stagproj.bank.controller.ContaController required a bean named 'entityManagerFactory' that could not be found.
-//
-//		The injection point has the following annotations:
-//		- @org.springframework.beans.factory.annotation.Autowired(required=true)
-//
-//
-//		Action:
-//
-//		Consider defining a bean named 'entityManagerFactory' in your configuration.
-//
-//
-//		Process finished with exit code 1
--- src/main/java/com/stagproj/bank/controller/ContaController.java
+++ src/main/java/com/stagproj/bank/controller/ContaController.java
@@ -1,13 +1,18 @@
 package com.stagproj.bank.controller;
 
 import com.stagproj.bank.repository.ContaRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+
+@RestController
 public class ContaController {
-    @Autowired
-    private ContaRepository contaRepository;
+
+    private final ContaRepository contaRepository;
+
+    public ContaController(ContaRepository contaRepository) {
+        this.contaRepository = contaRepository;
+    }
+
+
+
 }
