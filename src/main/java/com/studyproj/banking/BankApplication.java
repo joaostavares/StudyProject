e98fba824cc771b6ff72a261c2b98ae0dc54e430
João Tavares <joaostavares@outlook.com>
2023-01-13 10:21:45 -0300

    - `README.md` fix markdown - typos fix

--- src/main/java/com/studyproj/banking/BankApplication.java
+++ src/main/java/com/studyproj/banking/BankApplication.java
@@ -1,9 +1,12 @@
 package com.studyproj.banking;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@OpenAPIDefinition(info = @Info(title = "BankingAPI", version = "1.0", description = "Banking REST API Example"))
 @SpringBootApplication
 @EnableJpaRepositories("com.studyproj.banking.repositories")
 public class BankApplication {
